8f038a430305286a949555cf7202cf0ee3026910
Antonio Castiglione <antoniocastiglione10@gmail.com>
2019-09-30 17:24:00 -0400

    catching up and created site options thus far

--- page-home.php
+++ page-home.php
@@ -1,20 +1,16 @@
 <?php
 /*
  Template Name: Home Page Template
- *
- * This is going to be based on how I typically create home page templates.
- * It will have some default tie-ins with ACF for ease of use in the backend for those who arent
- * comfortable modifying templates directly. 
- * Of course you can scrap everything if it suits your use-case.
- * 
- * For more info: http://codex.wordpress.org/Page_Templates
 */
 ?>
 
 <?php get_header(); ?>
 
 <!-- Banner Area -->
-<section class="banner-section" style="background-image:url(<?php $img = get_field('banner_image'); if(!empty($img)) : echo $img['url']; else : echo get_template_directory_uri() . '/library/images/default-bg.jpg'; endif;  ?>);">
+<section class="banner-section" style="background-image:url(<?php $img = get_field('banner_image');
+																														if (!empty($img)) : echo $img['url'];
+																														else : echo get_template_directory_uri() . '/library/images/default-bg.jpg';
+																														endif;  ?>);">
 	<div class="banner-content-container wrap">
 		<h1 class="banner-title"><?php echo get_field('banner_title'); ?></h1>
 		<p class="banner-content"><?php echo get_field('banner_content'); ?></p>
@@ -27,35 +23,36 @@
 <!-- end Banner Area -->
 
 
-			<div id="content">
+<div id="content">
 
-				<div id="inner-content" class="wrap cf">
+	<div id="inner-content" class="wrap cf">
 
-						<main id="main" class="cf col-xs-12 col-sm-8 col-lg-8" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+		<main id="main" class="cf col-xs-12 col-sm-8 col-lg-8" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+					<article id="post-<?php the_ID(); ?>" <?php post_class('cf'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
-								<header class="article-header">
-									<h1 class="page-title"><?php the_title(); ?></h1>
-								</header>
+						<header class="article-header">
+							<h1 class="page-title"><?php the_title(); ?></h1>
+						</header>
 
-								<section class="entry-content cf" itemprop="articleBody">
-									<?php the_content(); ?>
-								</section>
+						<section class="entry-content cf" itemprop="articleBody">
+							<?php the_content(); ?>
+						</section>
 
-							</article>
+					</article>
 
-							<?php endwhile; endif; ?>
+			<?php endwhile;
+			endif; ?>
 
-						</main>
+		</main>
 
-						<?php get_sidebar(); ?>
+		<?php get_sidebar(); ?>
 
-				</div>
+	</div>
 
-			</div>
+</div>
 
 
 <?php get_footer(); ?>
