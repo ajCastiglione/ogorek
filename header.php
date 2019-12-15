168d6456c783c22a8a73fa0ec0c24f34311a05c4
Aj Castiglione <antoniocastiglione10@gmail.com>
2019-12-15 10:03:28 -0500

    updated tag manager code

--- header.php
+++ header.php
@@ -55,18 +55,24 @@
 
 	<?php // drop Google Analytics Here 
 	?>
-	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-45852794-1"></script>
+	<!-- Google Tag Manager -->
 	<script>
-		window.dataLayer = window.dataLayer || [];
-
-		function gtag() {
-			dataLayer.push(arguments);
-		}
-		gtag('js', new Date());
-
-		gtag('config', 'UA-45852794-1');
+		(function(w, d, s, l, i) {
+			w[l] = w[l] || [];
+			w[l].push({
+				'gtm.start': new Date().getTime(),
+				event: 'gtm.js'
+			});
+			var f = d.getElementsByTagName(s)[0],
+				j = d.createElement(s),
+				dl = l != 'dataLayer' ? '&l=' + l : '';
+			j.async = true;
+			j.src =
+				'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+			f.parentNode.insertBefore(j, f);
+		})(window, document, 'script', 'dataLayer', 'GTM-TRVGDQV');
 	</script>
+	<!-- End Google Tag Manager -->
 	<!-- Hotjar Tracking Code for ogorek.com -->
 	<script>
 		(function(h, o, t, j, a, r) {
@@ -90,7 +96,9 @@
 </head>
 
 <body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
-
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TRVGDQV" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
 	<div id="container">
 
 		<header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
