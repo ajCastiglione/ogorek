0d2f8d7f7ddd760c64005a0472c5f045ab39b994
Aj Castiglione <antoniocastiglione10@gmail.com>
2020-01-25 10:44:52 -0500

    working on landing page

---
+++ page-landing-marketing.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ Template Name: Landing Page for PPC
+*/
+?>
+
+<?php get_header(); ?>
+
+<div id="content" class="landing-page-content">
+
+  <div id="inner-content" class="cf">
+
+    <main id="main" class="cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+
+      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+          <article id="post-<?php the_ID(); ?>" <?php post_class('cf landing-page'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+
+            <?= get_template_part('partials/landing', 'hero') ?>
+
+            <?= get_template_part('template-parts/landing', 'marketing'); ?>
+
+          </article>
+
+      <?php endwhile;
+      endif; ?>
+
+    </main>
+
+  </div>
+
+</div>
+
+
+<?php get_footer(); ?>
---
+++ partials/landing-hero.php
@@ -0,0 +1,29 @@
+<?php
+$logo = get_field('logo', 'options')['url'];
+$hero = get_field('hero_image')['url'];
+$formID = get_field('contact_form');
+$form = do_shortcode("[gravityform id='{$formID}' title='false' description='false']");
+$title = get_field('above_form_title');
+$subtitle = get_field('above_form_subtitle');
+$formTitle = get_field('form_title');
+$formSubtitle = get_field('form_subtitle');
+?>
+
+<section class="hero-landing-page">
+    <div class="grid-50">
+        <div class="left" style="background-image:url(<?= $hero ?>)">
+            <img src="<?= $logo ?>" alt="Ogorek Wealth Management" class="logo">
+        </div>
+        <div class="right">
+            <h1 class="title"><?= $title ?></h1>
+            <h2 class="subtitle"><?= $subtitle ?></h2>
+            <div class="form">
+                <h2 class="form-title"><?= $formTitle ?></h2>
+                <h3 class="form-subtitle"><?= $formSubtitle ?></h3>
+                <?= $form ?>
+            </div>
+            <a href="#section-1" class="learn-more">learn more <svg class="down-arrow" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd">
+                    <path d="M23.245 4l-11.245 14.374-11.219-14.374-.781.619 12 15.381 12-15.391-.755-.609z" /></svg></a>
+        </div>
+    </div>
+</section>
