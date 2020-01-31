038909cabf894d2e749da14c6039e1167b6b2a9f
ajCastiglione <antoniocastiglione10@gmail.com>
2020-01-31 09:21:36 -0500

    added newsletter partial

---
+++ template-parts/footer-newsletter.php
@@ -0,0 +1,12 @@
+<?php
+$title = get_field('newsletter_title', 'options');
+$formID = get_field('newsletter_form_id', 'options');
+$form = do_shortcode("[gravityform id='{$formID}' title='false' description='false' ajax='true']");
+?>
+
+<section id="newsletter" class="newsletter-section">
+    <div class="wrap">
+        <h2 class="title"><?= $title ?></h2>
+        <?= $form ?>
+    </div>
+</section>
