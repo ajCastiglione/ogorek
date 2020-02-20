9df0b4a9e8b1e0fa6e259d2b65948811866be04e
Aj Castiglione <antoniocastiglione10@gmail.com>
2020-02-19 19:23:53 -0500

    updated blog to set ogorek team as the default poster if no author is set using the new system

--- post-formats/format.php
+++ post-formats/format.php
@@ -6,10 +6,12 @@
 
     <p class="byline entry-meta vcard">
 
-      <?php printf(
+      <?php
+      $post_author = get_field('post_author') ? get_the_title(get_field('post_author')[0]->ID) : get_the_author_meta('display_name', 2);
+      printf(
         __('', 'bonestheme') . ' %1$s %2$s %3$s',
         /* the time the post was published */
-        '<span class="by">' . __('by', 'bonestheme') . '</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link(get_the_author_meta('ID')) . '</span> | ',
+        '<span class="by">' . __('by', 'bonestheme') . '</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . $post_author . '</span> | ',
         /* the author of the post */
         '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time> | ',
         get_the_category_list(', ')
@@ -28,7 +30,7 @@
     <?php $a = new WP_Query(array('post_type' => 'team'));
     while ($a->have_posts()) : $a->the_post();
       $un = get_field('user_link')['user_nicename'];
-      if ($un === $nn) {
+      if ($un === $nn && !strstr($post_author, 'Team')) {
         $found = true; ?>
         <div class="author">
           <img src="<?= get_field('team_member_photo')['url'] ?>" alt="<?= the_title() ?>" class="portrait">
@@ -39,20 +41,15 @@
     endwhile;
     wp_reset_query();
     if (!$found && !empty(get_field('post_author'))) {
+      $found = true;
       $authorID = get_field('post_author')[0]->ID; ?>
       <div class="author">
         <img src="<?= get_field('team_member_photo', $authorID)['url'] ?>" alt="<?= the_title($authorID) ?>" class="portrait">
         <h2 class="name"><?= get_the_title($authorID) ?></h2>
         <a href="mailto:<?= get_field('email', $authorID) ?>" class="email"><?= get_field('email', $authorID) ?></a>
       </div>
-    <?php
-    } elseif (!$found) { ?>
-      <div class="author">
-        <h2 class="name">Ogorek Team</h2>
-        <a href="mailto:prosper@ogorek.com" class="email">prosper@ogorek.com</a>
-      </div>
     <?php } ?>
-    <div class="text">
+    <div class="text <?= $found ? '' : 'full-width' ?>">
       <?php
       the_content();
       ?>
