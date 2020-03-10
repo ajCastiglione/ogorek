4012515cfc9c41728f270e982896127f2e826485
Antonio Castiglione <antonio@minervawebdevelopment.com>
2020-03-10 18:12:15 -0400

    updated contact page to include more fields in admin and grid for contact info

--- functions.php
+++ functions.php
@@ -166,43 +166,8 @@ function bones_register_sidebars()
   ));
 } // don't remove this bracket!
 
-
-/************* COMMENT LAYOUT *********************/
-
-// Comment Layout
-function bones_comments($comment, $args, $depth)
-{
-  $GLOBALS['comment'] = $comment; ?>
-  <div id="comment-<?php comment_ID(); ?>" <?php comment_class('cf'); ?>>
-    <article class="cf">
-      <header class="comment-author vcard">
-        <?php // custom gravatar call 
-        ?>
-        <?php
-        // create variable
-        $bgauthemail = get_comment_author_email();
-        ?>
-        <img data-gravatar="http://www.gravatar.com/avatar/<?php echo md5($bgauthemail); ?>?s=40" class="load-gravatar avatar avatar-48 photo" height="40" width="40" src="<?php echo get_template_directory_uri(); ?>/library/images/nothing.gif" />
-        <?php // end custom gravatar call 
-        ?>
-        <?php printf(__('<cite class="fn">%1$s</cite> %2$s', 'bonestheme'), get_comment_author_link(), edit_comment_link(__('(Edit)', 'bonestheme'), '  ', '')) ?>
-        <time datetime="<?php echo comment_time('Y-m-j'); ?>"><a href="<?php echo htmlspecialchars(get_comment_link($comment->comment_ID)) ?>"><?php comment_time(__('F jS, Y', 'bonestheme')); ?> </a></time>
-
-      </header>
-      <?php if ($comment->comment_approved == '0') : ?>
-        <div class="alert alert-info">
-          <p><?php _e('Your comment is awaiting moderation.', 'bonestheme') ?></p>
-        </div>
-      <?php endif; ?>
-      <section class="comment_content cf">
-        <?php comment_text() ?>
-      </section>
-      <?php comment_reply_link(array_merge($args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
-    </article>
-    <?php // </li> is added by WordPress automatically 
-    ?>
-  <?php
-} // don't remove this bracket!
+// WP Comments
+require "theme/comments.php";
 
 function bones_fonts()
 {
@@ -220,6 +185,9 @@ function remove_protected_text()
 {
   return __('%s');
 }
+
+/************* ADD ROLES *********************/
+require "theme/acf-saves.php";
 
 /************* ADD ROLES *********************/
 require 'theme/add-roles.php';
@@ -248,4 +216,4 @@ require "theme/cron-jobs.php";
 
 
 
-/* DON'T DELETE THIS CLOSING TAG */ ?>
+/* DON'T DELETE THIS CLOSING TAG */
