7b6b23659c825b794216380c9bdf586651f191a6
Antonio Castiglione <antoniocastiglione10@gmail.com>
2020-03-11 11:07:25 -0400

    removed footer title

--- template-parts/landing-footer.php
+++ template-parts/landing-footer.php
@@ -5,7 +5,6 @@ $disclosures = get_field('disclosures', 'options');
 <footer class="landing-footer">
     <div id="inner-footer" class="cf col-1">
         <div class="content">
-            <h2 class="title">recognized by</h2>
             <img src="<?= $logos['url']; ?>" alt="Logos" class="logos">
         </div>
     </div>
