900828373ef2e266d928253d224d33f9ae45c691
Antonio Castiglione <antonio@minervawebdevelopment.com>
2020-04-19 21:59:58 -0400

    removed ajax from newsletter form to better track interactions

--- template-parts/footer-newsletter.php
+++ template-parts/footer-newsletter.php
@@ -1,7 +1,7 @@
 <?php
 $title = get_field('newsletter_title', 'options');
 $formID = get_field('newsletter_form_id', 'options');
-$form = do_shortcode("[gravityform id='{$formID}' title='false' description='false' ajax='true']");
+$form = do_shortcode("[gravityform id='{$formID}' title='false' description='false']");
 ?>
 
 <section id="newsletter" class="newsletter-section">
