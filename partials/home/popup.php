a7c125a07c9532d958e2f7afa1a415b7f66b2400
Antonio Castiglione <antonio@minervawebdevelopment.com>
2020-04-21 08:56:02 -0400

    added simple toggle to globally disable/enable popup

--- partials/home/popup.php
+++ partials/home/popup.php
@@ -2,15 +2,17 @@
 $logo = get_field('popup_logo', 'options');
 $title = get_field('popup_title', 'options');
 $content = get_field('popup_content', 'options');
+$show_popup = get_field('show_popup', 'options');
 ?>
-
-<section class="popup">
-    <div class="inner-popup large-wrapper">
-        <div class="close">X</div>
-        <img src="<?= $logo['url'] ?>" alt="<?= $logo['alt'] ?>" class="popup-logo">
-        <h2 class="popup-title"><?= $title ?></h2>
-        <div class="popup-content">
-            <?= $content ?>
+<?php if ($show_popup) : ?>
+    <section class="popup">
+        <div class="inner-popup large-wrapper">
+            <div class="close">X</div>
+            <img src="<?= $logo['url'] ?>" alt="<?= $logo['alt'] ?>" class="popup-logo">
+            <h2 class="popup-title"><?= $title ?></h2>
+            <div class="popup-content">
+                <?= $content ?>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
+<?php endif; ?>
