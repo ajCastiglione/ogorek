48f43880068efa09e7193175a8fd058c1b74013f
Antonio Castiglione <antonio@minervawebdevelopment.com>
2020-04-24 10:57:32 -0400

    fixed related info queries

--- theme/utils/get-related-posts.php
+++ theme/utils/get-related-posts.php
@@ -4,32 +4,33 @@ function get_related_posts($post)
   $related_posts = get_field('similar_posts', $post->ID);
   if (!empty($related_posts)) { ?>
     <div class="related-posts__container">
-      <?php foreach ($related_posts as $post) { ?>
+      <?php foreach ($related_posts as $related_post) { ?>
+
         <div class="related-posts__single">
-          <?php $link = get_field('external_link', $post->ID) ? get_field('link', $post->ID) : get_permalink($post->ID); ?>
-          <article id="post-<?php the_ID($post->ID); ?>" <?php post_class('cf post-card'); ?> role="article">
+          <?php $link = get_field('external_link', $related_post->ID) ? get_field('link', $related_post->ID) : get_permalink($related_post->ID); ?>
+          <article id="post-<?php the_ID($related_post->ID); ?>" <?php post_class('cf post-card'); ?> role="article">
 
             <header class="article-header">
 
               <div class="featured-image-container">
                 <a href="<?= $link ?>" class="post-link">
-                  <img src="<?= get_the_post_thumbnail_url($post->ID, 'large') ?>" alt="<?= the_title($post->ID) ?>" class="featured-image">
+                  <img src="<?= get_the_post_thumbnail_url($related_post->ID, 'large') ?>" alt="<?= the_title($related_post->ID) ?>" class="featured-image">
                   <span class="overlay"><i class="far fa-file-alt"></i></span>
                 </a>
               </div>
 
               <h2 class="h2 entry-title">
-                <a href="<?= $link ?>" rel="bookmark" title="<?php the_title_attribute($post->ID); ?>">
-                  <?php the_title(); ?>
+                <a href="<?= $link ?>" rel="bookmark" title="<?php the_title_attribute($related_post->ID); ?>">
+                  <?= get_the_title($related_post->ID); ?>
                 </a>
               </h2>
               <p class="byline entry-meta">
                 <?php printf(
                   __('', 'bonestheme') . ' %1$s %2$s',
                   /* the time the post was published */
-                  '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
+                  '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d', $related_post->ID) . '" itemprop="datePublished">' . get_the_time(get_option('date_format'), $related_post->ID) . '</time>',
                   /* the categories of the post */
-                  '<span class="by">' . __('|', 'bonestheme') . '</span> <span class="post-categories" itemprop="categories">' .  get_the_category_list(', ') . '</span>'
+                  '<span class="by">' . __('|', 'bonestheme') . '</span> <span class="post-categories" itemprop="categories">' .  get_the_category_list(', ', '', $related_post->ID) . '</span>'
                 ); ?>
               </p>
 
