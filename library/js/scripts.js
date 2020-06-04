abebd4847930bba0b5bde11f4ca7ca3bcad93cde
Antonio Castiglione <antonio@minervawebdevelopment.com>
2020-06-04 10:22:43 -0400

    updated contact page

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -372,6 +372,16 @@ function newsletterRedirect() {
   }
 }
 
+function contactNewsletter($) {
+  let btn = $("a[href*=letter]");
+  let form = $("#gform_wrapper_4");
+
+  btn.on("click", (e) => {
+    e.preventDefault();
+    form.show(500);
+  });
+}
+
 /*
  * Put all your regular jQuery in here.
  */
@@ -386,6 +396,10 @@ jQuery(document).ready(function ($) {
   mobileMenuSubMenu($);
   scrollTop($);
   newsletterRedirect();
+
+  if ($("body").hasClass("page-template-page-contact")) {
+    contactNewsletter($);
+  }
 
   if ($("body").hasClass("page-template-page-landing-marketing")) {
     showForm($);
