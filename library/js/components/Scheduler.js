19bba59949aa3ba2610a759b869cf3cdcefecc12
Antonio Castiglione <antonio@minervawebdevelopment.com>
2020-06-12 10:28:07 -0400

    updated so first name is capitalized

--- library/js/components/Scheduler.js
+++ library/js/components/Scheduler.js
@@ -1,19 +1,20 @@
 function prePopulateScheduler($) {
-  const urlParams = new URLSearchParams(window.location.search);
-  const firstName = urlParams.get("fname");
-  const lastName = urlParams.get("lname");
-  const email = urlParams.get("email");
-  const phone = urlParams.get("phone");
-  const scheduler = $("iframe[src*=calendly]");
-  let formattedURL;
-  if (firstName && lastName) {
-    formattedURL = `${scheduler.attr(
-      "src"
-    )}&name=${firstName}%20${lastName}&a1=${phone}&email=${email}`;
-    scheduler.attr("src", formattedURL);
-  } else {
-    return;
-  }
+    const urlParams = new URLSearchParams(window.location.search);
+    const firstNameParam = urlParams.get("fname");
+    const firstName = firstNameParam[0].toUpperCase() + firstNameParam.slice(1);
+    const lastName = urlParams.get("lname");
+    const email = urlParams.get("email");
+    const phone = urlParams.get("phone");
+    const scheduler = $("iframe[src*=calendly]");
+    let formattedURL;
+    if (firstName && lastName) {
+        formattedURL = `${scheduler.attr(
+            "src"
+        )}&name=${firstName}%20${lastName}&a1=${phone}&email=${email}`;
+        scheduler.attr("src", formattedURL);
+    } else {
+        return;
+    }
 }
 
 export default prePopulateScheduler;
