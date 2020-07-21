e47dca9ab5ee3424f90e41ae105b717add8a5795
Antonio Castiglione <antonio@minervawebdevelopment.com>
2020-07-21 16:46:47 -0400

    made into cards

--- page-videos.php
+++ page-videos.php
@@ -22,17 +22,30 @@ $query = new WP_Query($args);
 
       <?= get_template_part('partials/hero'); ?>
 
-      <article class="wrap">
+      <article class="video-commentaries">
 
         <?php if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
 
-            <section id="post-<?php the_ID(); ?>" <?php post_class('cf'); ?> role="article">
+            <div class="video-commentary">
+              <header class="video-header">
 
-              <h2 class="video-title"><?= the_title(); ?></h2>
+                <div class="featured-image-container">
+                  <a href="<?= the_permalink() ?>" class="video-link">
+                    <img src="<?= get_the_post_thumbnail_url($post->ID, 'large') ?>" alt="<?= the_title() ?>" class="video-image">
+                    <span class="overlay"><i class="far fa-file-alt"></i></span>
+                  </a>
+                </div>
 
-              <div class="video-content"><?= the_content(); ?></div>
+                <h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 
-            </section>
+              </header>
+
+              <section class="excerpt-content cf">
+                <a href="<?= the_permalink() ?>" class="video-link">
+                  <?php the_content(); ?>
+                </a>
+              </section>
+            </div>
 
         <?php endwhile;
         endif;
