6403efae6584b204ddbb39ddde1217a6d1d8c2a3
ajCastiglione <antonio@minervawebdevelopment.com>
2020-08-17 10:27:50 -0400

    updated per page, updated layout to show 3 across for laptop and up

--- page-videos.php
+++ page-videos.php
@@ -5,7 +5,7 @@
 $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 $args = array(
   'post_type' => 'video_commentaries',
-  'posts_per_page' => 6,
+  'posts_per_page' => 9,
   'paged' => $paged
 );
 
