cfbe28fbf4906504908447e037cb9076ccd579e1
Antonio Castiglione <antonio@minervawebdevelopment.com>
2020-08-25 10:43:05 -0400

    fixed newsletter breaking when a field is not filled out

--- library/js/components/ContactPage.js
+++ library/js/components/ContactPage.js
@@ -3,6 +3,10 @@ function contactNewsletter($) {
   let btn = $("a[href*=letter]");
   let form = $("#gform_wrapper_4");
 
+  if (form.hasClass("gform_validation_error")) {
+    form.show(300);
+  }
+
   btn.on("click", (e) => {
     e.preventDefault();
     form.show(500);
