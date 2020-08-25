3475eb0ccdac8f2a78301adeca3354d97aa4739a
Antonio Castiglione <antonio@minervawebdevelopment.com>
2020-08-25 11:33:55 -0400

    completed section 2 on homepage

--- page-home.php
+++ page-home.php
@@ -17,8 +17,9 @@
 					<article id="post-<?php the_ID(); ?>" <?php post_class('cf'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
 						<?= get_template_part('partials/home/banner'); ?>
-						<!-- popup -->
-						<?= get_template_part('partials/home/popup'); ?>
+
+						<?php # get_template_part('partials/home/popup'); 
+						?>
 
 						<?= get_template_part('partials/home/section-2'); ?>
 
