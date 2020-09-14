f35cea38827924e84891a0301ea89185c016c779
Antonio Castiglione <antonio@minervawebdevelopment.com>
2020-09-14 10:58:39 -0400

    tweaked hero

--- partials/hero.php
+++ partials/hero.php
@@ -23,7 +23,7 @@ if (is_page(460)) {
 
 <?php if (get_page_template_slug($post->ID) == 'page-secured.php') : ?>
   <div class="hero <?php echo $hero ? '' : 'no-image law-firm-hero' ?>" style="background-image:url(<?= $hero['url'] ?>)">
-    <?php $ogorek_logo = get_field('logo', 'options'); ?>
+    <?php $ogorek_logo = get_field('ogorek_company_logo', 'options'); ?>
     <div class="split-logo">
       <img src="<?= $ogorek_logo['url'] ?>" alt="Ogorek Wealth Management" class="logo">
       <?= !empty(get_field('law_firm_logo')) ? "<img class=\"firm-logo\" src=" . get_field('law_firm_logo')['url'] . ">" : "<h1 class=\"title\">" . get_the_title() . "</h1>" ?>
