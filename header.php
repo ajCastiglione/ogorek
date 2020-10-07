f4737dccd6fda24247e7534333e7649663a22a98
Antonio Castiglione <antonio@minervawebdevelopment.com>
2020-10-07 09:05:32 -0400

    updated so analytics is no longer called on local and dev sites

--- header.php
+++ header.php
@@ -54,52 +54,42 @@
 	<?php // end of wordpress head 
 	?>
 
-	<?php // drop Google Analytics Here 
+	<?php // drop Google Analytics Here
+	// Prevent analytics from loading on local and dev sites 
+	if (strpos($_SERVER['SERVER_NAME'], 'ogorek.com')) :
 	?>
-	<!-- Google Tag Manager -->
-	<script>
-		(function(w, d, s, l, i) {
-			w[l] = w[l] || [];
-			w[l].push({
-				'gtm.start': new Date().getTime(),
-				event: 'gtm.js'
-			});
-			var f = d.getElementsByTagName(s)[0],
-				j = d.createElement(s),
-				dl = l != 'dataLayer' ? '&l=' + l : '';
-			j.async = true;
-			j.src =
-				'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-			f.parentNode.insertBefore(j, f);
-		})(window, document, 'script', 'dataLayer', 'GTM-TRVGDQV');
-	</script>
-	<!-- End Google Tag Manager -->
-	<!-- Hotjar Tracking Code for ogorek.com -->
-	<script>
-		(function(h, o, t, j, a, r) {
-			h.hj = h.hj || function() {
-				(h.hj.q = h.hj.q || []).push(arguments)
-			};
-			h._hjSettings = {
-				hjid: 1613502,
-				hjsv: 6
-			};
-			a = o.getElementsByTagName('head')[0];
-			r = o.createElement('script');
-			r.async = 1;
-			r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-			a.appendChild(r);
-		})(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
-	</script>
+		<!-- Google Tag Manager -->
+		<script>
+			(function(w, d, s, l, i) {
+				w[l] = w[l] || [];
+				w[l].push({
+					'gtm.start': new Date().getTime(),
+					event: 'gtm.js'
+				});
+				var f = d.getElementsByTagName(s)[0],
+					j = d.createElement(s),
+					dl = l != 'dataLayer' ? '&l=' + l : '';
+				j.async = true;
+				j.src =
+					'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+				f.parentNode.insertBefore(j, f);
+			})(window, document, 'script', 'dataLayer', 'GTM-TRVGDQV');
+		</script>
+		<!-- End Google Tag Manager -->
 	<?php // end analytics 
+	endif;
 	?>
 
 </head>
 
 <body <?php body_class(detect_ie() ? 'ie' : ''); ?> itemscope itemtype="http://schema.org/WebPage">
-	<!-- Google Tag Manager (noscript) -->
-	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TRVGDQV" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-	<!-- End Google Tag Manager (noscript) -->
+
+	<?php if (strpos($_SERVER['SERVER_NAME'], 'ogorek.com')) : ?>
+		<!-- Google Tag Manager (noscript) -->
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TRVGDQV" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<!-- End Google Tag Manager (noscript) -->
+	<?php endif; ?>
+
 	<div id="container">
 
 		<header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
