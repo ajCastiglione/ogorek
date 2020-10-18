2e5847fff85865a605b06a5accb51722ffeb01f3
Antonio Castiglione <antonio@minervawebdevelopment.com>
2020-10-17 20:28:08 -0400

    added cta section and put s3 before s2

---
+++ partials/home/cta-section.php
@@ -0,0 +1,19 @@
+<?php
+
+$cta_title = get_field('cta_title');
+$cta_text = get_field('cta_text');
+$cta_button = get_field('cta_button');
+
+?>
+
+<section class="cta-section">
+    <div class="grid col-1">
+        <div class="cta-section__content">
+            <h2 class="cta-section__title"><?= $cta_title ?></h2>
+            <div class="cta-section__text"><?= $cta_text ?></div>
+        </div>
+        <div>
+            <a href="<?= $cta_button['link'] ?>" class="cta-section__btn"><?= $cta_button['title'] ?></a>
+        </div>
+    </div>
+</section>
