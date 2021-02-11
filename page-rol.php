e5f207eed84414951a5d990d7f8002e527a4ac50
Antonio Castiglione <antonio@minervawebdevelopment.com>
2021-02-11 16:39:24 -0500

    completed rol page

--- page-rol.php
+++ page-rol.php
@@ -16,7 +16,7 @@
 
 					<article id="post-<?php the_ID(); ?>" <?php post_class('cf'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
-						<?= get_template_part('template-parts/content', 'service'); ?>
+						<?= get_template_part('template-parts/content', 'rol'); ?>
 
 					</article>
 
