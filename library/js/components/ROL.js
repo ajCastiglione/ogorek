8b7a261638989d366321e703c9affdd136ddc3d0
Antonio Castiglione <antonio@minervawebdevelopment.com>
2021-02-20 09:07:20 -0500

    fixed positioning error when opening new article in popup

--- library/js/components/ROL.js
+++ library/js/components/ROL.js
@@ -1,20 +1,22 @@
 export default function ROL($) {
   const doc = $("html, body");
   const container = $(".article-popup-container");
-  const containerInner = $(".article-popup-container .content-wrap");
+  const containerInner = $(".article-popup-container .article-popup-inner");
+  const containerWrap = $(".article-popup-container .content-wrap");
   const posts = $(".related-posts__container .related-posts__single");
   const close = $(".article-popup-container .close-popup");
 
   const handlePopup = (e) => {
     e.preventDefault();
-    containerInner.empty();
+    containerWrap.empty();
     const target = $(e.target).closest(".related-posts__single");
     target
       .clone()
-      .appendTo(containerInner)
+      .appendTo(containerWrap)
       .find(".article-header")
       .wrapInner('<div class="content-wrap"></div>');
     container.removeClass("hidden");
+    containerInner.scrollTop(0);
     doc.css("overflow-y", "hidden");
   };
 
@@ -22,11 +24,11 @@ export default function ROL($) {
     if (string == "keypress" && (e.keyCode === 27 || e.which === 27)) {
       doc.css("overflow-y", "auto");
       container.addClass("hidden");
-      containerInner.empty();
+      containerWrap.empty();
     } else if (!string) {
       doc.css("overflow-y", "auto");
       container.addClass("hidden");
-      containerInner.empty();
+      containerWrap.empty();
     }
   };
 
